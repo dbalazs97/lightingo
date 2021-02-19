05f87ac9f5ed87a9c234746aa416c0f33794bb5f
Dóka Balázs <dbalazs40@gmail.com>
2021-02-19 14:16:56 +0100

    feat(repo): initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+	let spectator: Spectator<AppComponent>;
+	const createComponent = createComponentFactory({
+		component: AppComponent,
+		imports: [RouterTestingModule],
+	});
+
+	beforeEach(() => (spectator = createComponent()));
+
+	it('should create the app', () => {
+		expect(spectator.component).toBeTruthy();
+	});
+});
