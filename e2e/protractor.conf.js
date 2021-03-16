e73ab23a33dfd1c700da222d6aab2eb816ef984e
Dóka Balázs <dbalazs40@gmail.com>
2021-03-16 19:01:38 +0100

    feat(discovery): update packages

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,7 +9,7 @@ const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
  */
 exports.config = {
 	allScriptsTimeout: 11000,
-	specs: ['./src/**/*.e2e-spec.ts'],
+	specs: [ './src/**/*.e2e-spec.ts' ],
 	capabilities: {
 		browserName: 'chrome',
 	},
@@ -20,7 +20,8 @@ exports.config = {
 	jasmineNodeOpts: {
 		showColors: true,
 		defaultTimeoutInterval: 30000,
-		print: function () {},
+		print: function() {
+		},
 	},
 	onPrepare() {
 		require('ts-node').register({
@@ -31,7 +32,7 @@ exports.config = {
 				spec: {
 					displayStacktrace: StacktraceOption.PRETTY,
 				},
-			})
+			}),
 		);
 	},
 };
